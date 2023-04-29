ca0e68a38bf63a00a944c653f6bb7805fd46c781
hassan figuigui <hassan.figuigui7@aiesec.net>
2023-04-29 12:28:39 +0100

    configuring endpoint /api/blogs

--- blog/blogApi/views.py
+++ blog/blogApi/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
 # Create your views here.
+
+@api_view()
+def blogs(request):
+    return Response('list of blogs',
+                     status=status.HTTP_200_OK)
